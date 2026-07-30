a2cf1d4c282dc7733e1ddb6ff4a46f201ed15fee
TIRIVEEDI SOWMYA <22jr1a4493@gmail.com>
2026-07-30 18:49:37 +0530

    changes

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/FirstCode.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/FirstCode.java
@@ -1,5 +1,6 @@
 public class FirstCode {
     public static void main(String[] args) {
         System.out.println("hello i am trying how it work");
+        System.out.println("I am trying");
     }
 }
