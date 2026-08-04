0885fa968330e8ed5d4364aa9786834f8cfc04f5
TIRIVEEDI SOWMYA <22jr1a4493@gmail.com>
2026-08-04 19:44:07 +0530

    Weekly learning

---
+++ track/M02/T01/ST01/weeklyLearning.java
@@ -0,0 +1,21 @@
+package track.M02.T01.ST01;
+
+public class weeklyLearning {
+
+    public static void main(String[] args) {
+
+        int a = 17, b = 20, c = 3, d = 5;
+
+        double e = (double) a * 100 / b;
+
+        int f = b - a;
+
+        System.out.println("Completed Topics: " + a);
+
+        System.out.println("Remaining Topics: " + f);
+
+        System.out.println("Weekly Learning Hours: " + c * d);
+
+        System.out.println("Progress Percentage: " + e);
+    }
+}
