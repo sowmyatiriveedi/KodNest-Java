3514a7dd8cf505d3d81900ea18b192a8556d490f
TIRIVEEDI SOWMYA <22jr1a4493@gmail.com>
2026-08-04 16:34:34 +0530

    code 2 in mod 2

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/M02/T01/ST01/Main.java
@@ -0,0 +1,8 @@
+package M02.T01.ST01;
+
+public class Main {
+    Public static void main(String[] args) {
+        System.out.println("Hello KodNest");
+    }
+
+}
---
+++ track/M02/T01/ST01/practiceHours.java
@@ -0,0 +1,10 @@
+package track.M02.T01.ST01;
+
+public class practiceHours {
+    public static void main(String[] args) {
+        String learnerName = "Salman";
+        int dailyPracticeHours = 8;
+        System.out.println("Name: " + learnerName);
+        System.out.println("Daily Practice:" + dailyPracticeHours + "Hours");
+    }
+}
