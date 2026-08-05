abf3b35e06ed7e3fdf9dbb38cc23473eca9b995a
TIRIVEEDI SOWMYA <22jr1a4493@gmail.com>
2026-08-05 16:49:40 +0530

    placement eligible

---
+++ track/M02/T02/ST01/placementEligible.java
@@ -0,0 +1,15 @@
+package track.M02.T02.ST01;
+
+public class placementEligible {
+    public static void main(String[] args) {
+        int marks = 72;
+        int attendance = 80;
+        boolean projectCompleted = true;
+
+        boolean marksEligible = marks >= 60;
+        boolean attendanceEligible = attendance >= 75;
+        boolean academicEligible = marksEligible && attendanceEligible;
+        boolean placementEligible = academicEligible && projectCompleted;
+        System.out.println("Placement Eligible: " + placementEligible);
+    }
+}
