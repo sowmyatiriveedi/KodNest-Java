5d96a1810af9082d81ed8a13ed2f0f5eab1878e6
TIRIVEEDI SOWMYA <22jr1a4493@gmail.com>
2026-08-05 15:44:52 +0530

    largest no

---
+++ track/M02/T02/ST01/st03.java
@@ -0,0 +1,14 @@
+package track.M02.T02.ST01;
+
+public class st03 {
+    public static void main(String[] args) {
+        int n1 = 19;
+        int n2 = 25;
+        if (n1 >= n2) {
+            System.out.println("Larger: " + n1);
+        } else {
+            System.out.println("Larger: " + n2);
+        }
+    }
+
+}
