14cb180b23bbd0190dcbbacacb08ac47db4f45f8
TIRIVEEDI SOWMYA <22jr1a4493@gmail.com>
2026-08-05 14:39:29 +0530

    class practice

---
+++ track/M02/T01/ST01/conditionallooppactice.java
@@ -0,0 +1,13 @@
+package track.M02.T01.ST01;
+
+public class conditionallooppactice {
+
+    public static void main(String[] args) {
+        int num = 20;
+        if (num > 15) {
+            System.out.println("num is greater");
+        } else {
+            System.out.println("num is smaller");
+        }
+    }
+}
