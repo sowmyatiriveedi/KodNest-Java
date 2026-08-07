e26eea0172d8e68bd0ded8bae9987d4f93dec806
TIRIVEEDI SOWMYA <22jr1a4493@gmail.com>
2026-08-07 15:00:45 +0530

    continue

---
+++ track/M02/T02/ST01/continuefor.java
@@ -0,0 +1,13 @@
+package track.M02.T02.ST01;
+
+public class continuefor {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            if (i == 3) {
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+
+}
