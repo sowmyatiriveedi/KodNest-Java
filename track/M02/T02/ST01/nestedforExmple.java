d25369711643651ddd5ba84e553ce159ea328db8
TIRIVEEDI SOWMYA <22jr1a4493@gmail.com>
2026-08-07 14:44:44 +0530

    nested for loop

---
+++ track/M02/T02/ST01/nestedforExmple.java
@@ -0,0 +1,13 @@
+package track.M02.T02.ST01;
+
+public class nestedforExmple {
+    public static void main(String[] args) {
+        int i, j;
+        for (i = 1; i <= 5; i++) {
+            for (j = 1; j <= 5; j++) {
+                System.out.println(j);
+            }
+            System.out.println();
+        }
+    }
+}
