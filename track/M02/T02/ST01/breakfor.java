8b7e714a9d023180a098cb8308ab368dcb5039ce
TIRIVEEDI SOWMYA <22jr1a4493@gmail.com>
2026-08-07 14:58:43 +0530

    break

---
+++ track/M02/T02/ST01/breakfor.java
@@ -0,0 +1,14 @@
+package track.M02.T02.ST01;
+
+public class breakfor {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            if (i == 3) {
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+
+}
