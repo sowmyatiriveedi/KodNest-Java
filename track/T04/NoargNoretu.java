1db114a9ecee26e10440d72d6e523e7c690385ff
TIRIVEEDI SOWMYA <22jr1a4493@gmail.com>
2026-08-11 15:34:05 +0530

    no argument no return type

---
+++ track/T04/NoargNoretu.java
@@ -0,0 +1,13 @@
+package track.T04;
+
+public class NoargNoretu {
+    public static void main(String[] args) {
+        greet();
+
+    }
+
+    static void greet() {
+        System.out.println("Hello");
+    }
+
+}
