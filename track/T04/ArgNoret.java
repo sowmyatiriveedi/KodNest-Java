1440d7cfd406612ebbaf602e98221ccad2d3d145
TIRIVEEDI SOWMYA <22jr1a4493@gmail.com>
2026-08-11 15:36:42 +0530

    with argument no return type

---
+++ track/T04/ArgNoret.java
@@ -0,0 +1,13 @@
+package track.T04;
+
+public class ArgNoret {
+    public static void main(String[] args) {
+        add(10, 20);
+    }
+
+    static void add(int a, int b) {
+        int r = a + b;
+        System.out.println(r);
+    }
+
+}
