ac7550a9bfbeb61b0fb33602687635e30e2f88bb
TIRIVEEDI SOWMYA <22jr1a4493@gmail.com>
2026-08-14 16:26:53 +0530

    Product Details

---
+++ track/M02/T06/ProductDetails.java
@@ -0,0 +1,28 @@
+package track.M02.T06;
+
+import java.util.Scanner;
+
+class Product {
+    String name;
+    double price;
+
+    Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    void display() {
+        System.out.println("Product " + name);
+        System.out.println("Price: " + price);
+    }
+}
+
+public class ProductDetails {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        double price = scanner.nextDouble();
+        Product p = new Product(name, price);
+        p.display();
+    }
+}
