884c0f5f9380e9d9a69d7ad336f453c4423c09fa
TIRIVEEDI SOWMYA <22jr1a4493@gmail.com>
2026-08-14 15:26:02 +0530

    Constructor

---
+++ track/M02/T06/Constructor.java
@@ -0,0 +1,27 @@
+package track.M02.T06;
+
+class Student {
+    String name;
+    int age;
+    double height;
+
+    public Student(String name, int age, double height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
+    void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+    }
+}
+
+public class Constructor {
+    public static void main(String[] args) {
+        Student s1 = new Student("sowmya", 20, 5.5);
+        // s1.input("sowmya", 20, 5.5);
+        s1.display();
+    }
+}
