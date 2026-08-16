37a1007803be5958da604b24ef9723faa4c88ca5
TIRIVEEDI SOWMYA <22jr1a4493@gmail.com>
2026-08-16 14:54:30 +0530

    constructor with parameters

---
+++ track/M02/T06/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class Book {
+    String title;
+    String author;
+
+    Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    void display() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        {
+            String title = scanner.nextLine();
+            String author = scanner.nextLine();
+            Book b = new Book(title, author);
+            b.display();
+        }
+    }
+}
