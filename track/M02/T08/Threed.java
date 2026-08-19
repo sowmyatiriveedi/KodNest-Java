bc6f7f96b6a72064f3245c35030eb74beb7f9a07
TIRIVEEDI SOWMYA <22jr1a4493@gmail.com>
2026-08-19 16:46:47 +0530

    3d array

---
+++ track/M02/T08/Threed.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Threed{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a[][][] = new int[2][2][2];
+        for(int i=0;i<a.length;i++){
+            for(int j=0;j<a[i].length;j++){
+                for(int k=0;k<a[i][j].length;k++){
+                    a[i][j][k] = s.nextInt();
+                }
+            }
+        }
+        for(int i=0;i<a.length;i++){
+            for(int j=0;j<a[i].length;j++){
+                for(int k=0;k<a[i][j].length;k++){
+                    System.out.print(a[i][j][k]+" ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+}
