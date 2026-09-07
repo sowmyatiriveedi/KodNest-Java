5e58995f6fe3d872aebed5044e74a2de83afe52d
TIRIVEEDI SOWMYA <22jr1a4493@gmail.com>
2026-09-07 19:43:01 +0530

    value equal or not

---
+++ track/M04/T01/s4.java
@@ -0,0 +1,14 @@
+package track.M04.T01;
+
+public class s4 {
+    public static void main(String[] args) {
+        String a = new String("java");
+        String b = new String("java");
+        if (a.equals(b))
+            System.out.println("equals");
+        else
+            System.out.println("not");
+
+    }
+
+}
