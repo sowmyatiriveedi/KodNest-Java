01b26070a110bcc9a8981140e47c2dc7c5e1f0e3
TIRIVEEDI SOWMYA <22jr1a4493@gmail.com>
2026-09-07 19:39:01 +0530

    value checking

---
+++ track/M04/T01/s3.java
@@ -0,0 +1,13 @@
+package track.M04.T01;
+
+public class s3 {
+    public static void main(String[] args) {
+        String s1 = "java";
+        String s2 = "java";
+        if (s1.equals(s2))
+            System.out.println("equal");
+        else
+            System.out.println("not");
+
+    }
+}
