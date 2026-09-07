6b622428874a3106dff06a22243ef72a3775b048
TIRIVEEDI SOWMYA <22jr1a4493@gmail.com>
2026-09-07 19:32:44 +0530

    check object references

---
+++ track/M04/T01/S1.java
@@ -0,0 +1,16 @@
+package track.M04.T01;
+
+public class S1 {
+    public static void main(String[] args) {
+        {
+            String a = "Java";
+            String b = "Java";
+            if (a == b) {
+                System.out.println("ref equal");
+            } else {
+                System.out.println("not equal");
+            }
+        }
+    }
+
+}
---
+++ track/M04/T01/s2.java
@@ -0,0 +1,13 @@
+package track.M04.T01;
+
+public class s2 {
+    public static void main(String[] args) {
+        String a = new String("Java");
+        String b = new String("Java");
+        if (a == b)
+            System.out.println("equal");
+        else
+            System.out.println("not equal");
+    }
+
+}
